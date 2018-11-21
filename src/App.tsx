3a411e788568e24c6d05ab407ca78f6a1fb7f4b8
Micah Bales <micahbales@gmail.com>
2018-11-20 21:30:06 -0500

    Display Numbers When Buttons Are Pressed
    Also, add a few more buttons.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,71 @@
 import * as React from 'react';
+import { AppState } from './definitions';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props: Object) {
+    super(props);
+    this.numButtonPress = this.numButtonPress.bind(this);
+    this.clearButtonPress = this.clearButtonPress.bind(this);
+  }
+
+  state: AppState = {
+    displayValue: '0',
+    total: 0,
+  };
+
+  numButtonPress(e: React.SyntheticEvent) {
+    const state: AppState = Object.assign({}, this.state);
+    const buttonValue: string = e.currentTarget.textContent ?
+        e.currentTarget.textContent : '';
+    state.displayValue = state.displayValue === '0' ?
+        buttonValue :
+        state.displayValue += buttonValue;
+    this.setState(state);
+  }
+
+  clearButtonPress(e: React.SyntheticEvent) {
+    e.preventDefault();
+
+    const state = Object.assign({}, this.state);
+    state.total = 0;
+    state.displayValue = '0';
+    this.setState(state);
+  }
+
   public render() {
     return (
       <div className="app">
         <div className="calc">
           <div className="calc__row display">
-            <span className="total">0</span>
+            <span className="total">{this.state.displayValue}</span>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__clear">clear</div>
+            <div className="button calc__clear" onClick={this.clearButtonPress}>clear</div>
             <div className="button calc__operator">/</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num">7</div>
-            <div className="button calc__num">8</div>
-            <div className="button calc__num">9</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>7</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>8</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>9</div>
+            <div className="button calc__operator">x</div>
+          </div>
+          <div className="calc__row buttons">
+            <div className="button calc__num" onClick={this.numButtonPress}>4</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>5</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>6</div>
             <div className="button calc__operator">-</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num">4</div>
-            <div className="button calc__num">5</div>
-            <div className="button calc__num">6</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>1</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>2</div>
+            <div className="button calc__num" onClick={this.numButtonPress}>3</div>
             <div className="button calc__operator">+</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num">1</div>
-            <div className="button calc__num">2</div>
-            <div className="button calc__num">3</div>
+            <div className="button calc__num zero" onClick={this.numButtonPress}>0</div>
+            <div className="button" onClick={this.numButtonPress}>.</div>
             <div className="button calc__operator">=</div>
           </div>
         </div>
