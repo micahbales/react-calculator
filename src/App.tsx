47e0dcc6c42880ae369d6b24a637e94c8ad3cebe
Micah Bales <micahbales@gmail.com>
2018-11-22 22:57:37 -0500

    Allow Keyboard Inputs to Calculator

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './App.css';
 
 class App extends React.Component {
 
+  numberKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+  operatorKeys = ['+', '=', '*', '/', '-'];
   operate = {
     '*': (x: number, y: number) => Math.abs(x * y),
     '/': (x: number, y: number) => Math.abs(x / y),
@@ -11,11 +13,28 @@ class App extends React.Component {
     '-': (x: number, y: number) => Math.abs(x - y),
   };
 
+  static limitDecimals(displayValue: string) {
+    const val = Number(displayValue);
+    return Math.round(val * 1000) / 1000;
+  }
+
+  componentWillMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+
   constructor(props: Object) {
     super(props);
-    this.numButtonPress = this.numButtonPress.bind(this);
+
+    this.handleInput = this.handleInput.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleButtonPress = this.handleButtonPress.bind(this);
     this.clearButtonPress = this.clearButtonPress.bind(this);
     this.operatorButtonPress = this.operatorButtonPress.bind(this);
+    this.updateDisplayValue = this.updateDisplayValue.bind(this);
   }
 
   state: AppState = {
@@ -26,13 +45,29 @@ class App extends React.Component {
     total: 0,
   };
 
-  numButtonPress(e: React.SyntheticEvent) {
+  handleKeyDown(e: KeyboardEvent) {
+    if (e.key === 'c') return this.clearButtonPress(e);
+    this.handleInput(e.key);
+  }
+
+  handleButtonPress(e: React.SyntheticEvent) {
+    const buttonValue: string = e.currentTarget.textContent ? e.currentTarget.textContent : '';
+    this.handleInput(buttonValue);
+  }
+
+  handleInput(key: string) {
+    const isUpdateKey = this.numberKeys.includes(key);
+    if (isUpdateKey) this.updateDisplayValue(key);
+
+    const isOperatorKey = this.operatorKeys.includes(key);
+    if (isOperatorKey) this.operatorButtonPress(key);
+  }
+
+  updateDisplayValue(buttonValue: string) {
     const state: AppState = Object.assign({}, this.state);
-    const buttonValue: string = e.currentTarget.textContent ?
-        e.currentTarget.textContent : '';
 
     // Only allow one '.' character in the display
-    if (buttonValue.charCodeAt(0) === 46 && state.displayValue.includes('.')) return;
+    if (buttonValue === '.' && state.displayValue.includes('.')) return;
 
     // Determine whether to append numbers to the display, or start fresh
     state.displayValue = (state.displayValue === '0' || state.operatorPressed) ?
@@ -44,8 +79,8 @@ class App extends React.Component {
     this.setState(state);
   }
 
-  operatorButtonPress(e: React.SyntheticEvent) {
-    const operator: string = e.currentTarget.textContent ? e.currentTarget.textContent : '';
+  operatorButtonPress(buttonValue: string) {
+    const operator = buttonValue;
     const state: AppState = Object.assign({}, this.state);
     const operatorIsEquals = operator && operator.charCodeAt(0) === 61;
 
@@ -63,7 +98,7 @@ class App extends React.Component {
     this.setState(state);
   }
 
-  clearButtonPress(e: React.SyntheticEvent) {
+  clearButtonPress(e: React.SyntheticEvent | KeyboardEvent) {
     e.preventDefault();
 
     const state = Object.assign({}, this.state);
@@ -80,34 +115,34 @@ class App extends React.Component {
       <div className="app">
         <div className="calc">
           <div className="calc__row display">
-            <span className="total">{this.state.displayValue}</span>
+            <span className="total">{App.limitDecimals(this.state.displayValue)}</span>
           </div>
           <div className="calc__row buttons">
             <div className="button calc__clear" onClick={this.clearButtonPress}>clear</div>
-            <div className="button calc__operator" onClick={this.operatorButtonPress}>/</div>
+            <div className="button calc__operator" onClick={this.handleButtonPress}>/</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num" onClick={this.numButtonPress}>7</div>
-            <div className="button calc__num" onClick={this.numButtonPress}>8</div>
-            <div className="button calc__num" onClick={this.numButtonPress}>9</div>
-            <div className="button calc__operator" onClick={this.operatorButtonPress}>*</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>7</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>8</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>9</div>
+            <div className="button calc__operator" onClick={this.handleButtonPress}>*</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num" onClick={this.numButtonPress}>4</div>
-            <div className="button calc__num" onClick={this.numButtonPress}>5</div>
-            <div className="button calc__num" onClick={this.numButtonPress}>6</div>
-            <div className="button calc__operator" onClick={this.operatorButtonPress}>-</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>4</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>5</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>6</div>
+            <div className="button calc__operator" onClick={this.handleButtonPress}>-</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num" onClick={this.numButtonPress}>1</div>
-            <div className="button calc__num" onClick={this.numButtonPress}>2</div>
-            <div className="button calc__num" onClick={this.numButtonPress}>3</div>
-            <div className="button calc__operator" onClick={this.operatorButtonPress}>+</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>1</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>2</div>
+            <div className="button calc__num" onClick={this.handleButtonPress}>3</div>
+            <div className="button calc__operator" onClick={this.handleButtonPress}>+</div>
           </div>
           <div className="calc__row buttons">
-            <div className="button calc__num zero" onClick={this.numButtonPress}>0</div>
-            <div className="button" onClick={this.numButtonPress}>.</div>
-            <div className="button calc__operator" onClick={this.operatorButtonPress}>=</div>
+            <div className="button calc__num zero" onClick={this.handleButtonPress}>0</div>
+            <div className="button" onClick={this.handleButtonPress}>.</div>
+            <div className="button calc__operator" onClick={this.handleButtonPress}>=</div>
           </div>
         </div>
       </div>
